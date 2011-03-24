6506868f16c86bba781d12e2045ece00df34dd93
warfares <rodolfo.barriga@gmail.com>
2011-03-24 12:01:40 -0400

    added util relative time fc

--- src/html_view/content/js/app/util.js
+++ src/html_view/content/js/app/util.js
@@ -226,7 +226,46 @@ Philosophy.Util = {
 		};
 		return PGtypes[t];
 	}
-};   // eo Philosophy.Util
+	,
+	relativeTime: function(date, now_threshold) {
+		var delta = new Date() - date;
+
+		now_threshold = parseInt(now_threshold, 10);
+
+		if (isNaN(now_threshold)) {
+			now_threshold = 0;
+		}
+
+		if (delta <= now_threshold) {
+			return 'Just now';
+		}
+
+		var units = null;
+		var conversions = {
+			millisecond: 1, // ms    -> ms
+			second: 1000,   // ms    -> sec
+			minute: 60,     // sec   -> min
+			hour:   60,     // min   -> hour
+			day:    24,     // hour  -> day
+			month:  30,     // day   -> month (roughly)
+			year:   12      // month -> year
+		};
+
+		for (var key in conversions) {
+			if (delta < conversions[key]) {
+				break;
+			} else {
+				units = key; // keeps track of the selected key over the iteration
+				delta = delta / conversions[key];
+			}
+		}
+
+		// pluralize a unit when the difference is greater than 1.
+		delta = Math.floor(delta);
+		if (delta !== 1) { units += "s"; }
+		return [delta, units, "ago"].join(" ");
+	}
+};// eo Philosophy.Util
 //
 
 Philosophy.Msg = Philosophy.Util.popupMessage;
